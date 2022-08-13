889ca0d364ecaba995b49c6c4a04268ce745e902
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-08-14 00:10:09 +0530

    home_view: add bottom navigation bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,7 +5,18 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  static const TextStyle _textStyle = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 2,
+  );
+
+  List<Widget> pages = [
+    Text('Home'),
+    Text('Profile Page Noice'),
+  ];
+
+  HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,12 +24,29 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: GetX<HomeController>(builder: (controller) {
+        return Center(
+          child: pages[Get.find<HomeController>().currentIndex.value],
+        );
+      }),
+      bottomNavigationBar: GetX<HomeController>(builder: (controller) {
+        return NavigationBar(
+          selectedIndex: Get.find<HomeController>().currentIndex.value,
+          onDestinationSelected: (int newIndex) {
+            Get.find<HomeController>().currentIndex.value = newIndex;
+          },
+          destinations: const [
+            NavigationDestination(
+                selectedIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: 'Home'),
+            NavigationDestination(
+                selectedIcon: Icon(Icons.account_circle),
+                icon: Icon(Icons.account_circle_outlined),
+                label: 'Profile'),
+          ],
+        );
+      }),
     );
   }
 }
