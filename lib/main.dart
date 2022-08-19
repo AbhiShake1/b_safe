6f48349bc807b77dfd1655c572f46274ce2c80ec
abhi <abhi9848038283@gmail.com>
2022-08-19 21:47:16 +0545

    integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:b_safe/app/routes/app_pages.dart';
+import 'package:b_safe/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
-import 'app/routes/app_pages.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: 'BeSafe',
