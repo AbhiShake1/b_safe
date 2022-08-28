fd3ce5f2955b2bf380f53b48e20761f82148c449
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-08-28 15:24:11 +0530

    make navbar transparent, complete login view

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,17 +9,15 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-          // Scaffold(
-          // backgroundColor: Colors.white,
-          // body:
           SafeArea(
         child: SingleChildScrollView(
           child: Container(
             height: MediaQuery.of(context).size.height,
             child: Column(
               children: <Widget>[
+                SizedBox(height: 50),
                 Expanded(
-                  flex: 2,
+                  flex: 3,
                   child: Column(
                     children: <Widget>[
                       const SizedBox(height: 35),
@@ -84,33 +82,119 @@ class LoginView extends GetView<LoginController> {
                     ],
                   ),
                 ),
+                SizedBox(height: 20),
                 Expanded(
-                  flex: 1,
+                  flex: 3,
                   child: Column(
                     children: <Widget>[
                       Container(
                           constraints: const BoxConstraints(maxWidth: 500),
                           margin: const EdgeInsets.symmetric(horizontal: 10),
-                          child: RichText(
+                          child: Text(
+                            'We will be sending you an OTP on this mobile number.',
                             textAlign: TextAlign.center,
-                            text: TextSpan(children: <TextSpan>[
-                              TextSpan(text: 'We will send you an '),
-                              TextSpan(text: 'One Time Password '),
-                              TextSpan(text: 'on this mobile number'),
-                            ]),
-                          )),
-                      Container(
-                        height: 40,
-                        constraints: const BoxConstraints(maxWidth: 500),
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        child: Text('Hi'),
+                            style: GoogleFonts.poppins(
+                              letterSpacing: 2,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            textStyle: const TextStyle(
+                              shadows: [
+                                Shadow(
+                                  color: Color.fromRGBO(93, 6, 138, 150),
+                                  blurRadius: 10.0,
+                                  offset: Offset(5.0, 5.0),
+                                ),
+                                Shadow(
+                                  color: Color.fromRGBO(93, 6, 138, 150),
+                                  blurRadius: 10.0,
+                                  offset: Offset(-5.0, 5.0),
+                                ),
+                              ],
+                            )),
+                          ),
                       ),
+                      SizedBox(height: 20),
                       Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        constraints: const BoxConstraints(maxWidth: 500),
-                        child: Text("Hi"),
+                        // height: 45,
+                        // constraints: const BoxConstraints(
+                        //     maxWidth: 500
+                        // ),
+                        margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                        child: const TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                ),
+                                hintText: '+91',
+
+
+                            )
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        constraints: const BoxConstraints(
+                            maxWidth: 500
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Color.fromRGBO(93, 6, 138, 160))
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  'Next',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                                    color: Color.fromRGBO(93, 6, 138, 150),
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                    size: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        // RaisedButton(
+                        //   onPressed: () {},
+                        //   color: Colors.purpleAccent
+                        //   shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(14))),
+                        //   child: Container(
+                        //     padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                        //     child: Row(
+                        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        //       children: <Widget>[
+                        //         Text(
+                        //           'Next',
+                        //           style: TextStyle(color: Colors.white),
+                        //         ),
+                        //         Container(
+                        //           padding: const EdgeInsets.all(8),
+                        //           decoration: BoxDecoration(
+                        //             borderRadius: const BorderRadius.all(Radius.circular(20)),
+                        //             color: MyColors.primaryColorLight,
+                        //           ),
+                        //           child: Icon(
+                        //             Icons.arrow_forward_ios,
+                        //             color: Colors.white,
+                        //             size: 16,
+                        //           ),
+                        //         )
+                        //       ],
+                        //     ),
+                        //   ),
+                        // ),
                       )
                     ],
                   ),
