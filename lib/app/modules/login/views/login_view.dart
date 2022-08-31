30f3a098e6118e9901f3260e4f209754f764f7bc
abhi <abhi9848038283@gmail.com>
2022-08-31 20:47:31 +0545

    minor refactor

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -8,14 +8,13 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
+          child: SizedBox(
             height: MediaQuery.of(context).size.height,
             child: Column(
               children: <Widget>[
-                SizedBox(height: 50),
+                const SizedBox(height: 50),
                 Expanded(
                   flex: 3,
                   child: Column(
@@ -23,7 +22,9 @@ class LoginView extends GetView<LoginController> {
                       const SizedBox(height: 35),
                       Container(
                         margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 20),
+                          horizontal: 20,
+                          vertical: 20,
+                        ),
                         child: Stack(
                           alignment: Alignment.center,
                           textDirection: TextDirection.rtl,
@@ -36,9 +37,10 @@ class LoginView extends GetView<LoginController> {
                                     const BoxConstraints(maxWidth: 500),
                                 // margin: const EdgeInsets.only(top: 100),
                                 decoration: const BoxDecoration(
-                                    color: Color.fromRGBO(93, 6, 138, 210),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(30))),
+                                  color: Color.fromRGBO(93, 6, 138, 210),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(30)),
+                                ),
                               ),
                             ),
                             Center(
@@ -46,9 +48,10 @@ class LoginView extends GetView<LoginController> {
                                 clipBehavior: Clip.hardEdge,
                                 constraints:
                                     const BoxConstraints(maxHeight: 250),
-                                decoration: BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(60))),
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(60)),
+                                ),
                                 margin:
                                     const EdgeInsets.symmetric(horizontal: 8),
                                 child: Image.asset('assets/women.jpg'),
@@ -60,103 +63,112 @@ class LoginView extends GetView<LoginController> {
                       Text(
                         'B SAFE',
                         style: GoogleFonts.limelight(
-                            fontWeight: FontWeight.w900,
-                            fontSize: 50,
-                            textStyle: const TextStyle(
-                              shadows: [
-                                Shadow(
-                                  color: Color.fromRGBO(93, 6, 138, 150),
-                                  blurRadius: 10.0,
-                                  offset: Offset(5.0, 5.0),
-                                ),
-                                Shadow(
-                                  color: Color.fromRGBO(93, 6, 138, 150),
-                                  blurRadius: 10.0,
-                                  offset: Offset(-5.0, 5.0),
-                                ),
-                              ],
-                            ),
-                            letterSpacing: 6,
-                            color: Color.fromRGBO(34, 29, 58, 85)),
+                          fontWeight: FontWeight.w900,
+                          fontSize: 50,
+                          textStyle: const TextStyle(
+                            shadows: [
+                              Shadow(
+                                color: Color.fromRGBO(93, 6, 138, 150),
+                                blurRadius: 10,
+                                offset: Offset(5, 5),
+                              ),
+                              Shadow(
+                                color: Color.fromRGBO(93, 6, 138, 150),
+                                blurRadius: 10,
+                                offset: Offset(-5, 5),
+                              ),
+                            ],
+                          ),
+                          letterSpacing: 6,
+                          color: const Color.fromRGBO(34, 29, 58, 85),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Expanded(
                   flex: 3,
                   child: Column(
                     children: <Widget>[
                       Container(
-                          constraints: const BoxConstraints(maxWidth: 500),
-                          margin: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Text(
-                            'We will be sending you an OTP on this mobile number.',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.poppins(
-                              letterSpacing: 2,
+                        constraints: const BoxConstraints(maxWidth: 500),
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'We will be sending you an OTP on this mobile number.',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                            letterSpacing: 2,
                             fontWeight: FontWeight.w600,
                             fontSize: 15,
                             textStyle: const TextStyle(
                               shadows: [
                                 Shadow(
                                   color: Color.fromRGBO(93, 6, 138, 150),
-                                  blurRadius: 10.0,
-                                  offset: Offset(5.0, 5.0),
+                                  blurRadius: 10,
+                                  offset: Offset(5, 5),
                                 ),
                                 Shadow(
                                   color: Color.fromRGBO(93, 6, 138, 150),
-                                  blurRadius: 10.0,
-                                  offset: Offset(-5.0, 5.0),
+                                  blurRadius: 10,
+                                  offset: Offset(-5, 5),
                                 ),
                               ],
-                            )),
+                            ),
                           ),
+                        ),
                       ),
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       Container(
                         // height: 45,
                         // constraints: const BoxConstraints(
                         //     maxWidth: 500
                         // ),
-                        margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 22,
+                          vertical: 10,
+                        ),
                         child: const TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                ),
-                                hintText: '+91',
-
-
-                            )
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: '+91',
+                          ),
                         ),
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        constraints: const BoxConstraints(
-                            maxWidth: 500
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
                         ),
+                        constraints: const BoxConstraints(maxWidth: 500),
                         child: ElevatedButton(
                           onPressed: () {},
                           style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(Color.fromRGBO(93, 6, 138, 160))
+                            backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(93, 6, 138, 160),
+                            ),
                           ),
                           child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 8,
+                            ),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: <Widget>[
-                                Text(
+                                const Text(
                                   'Next',
                                   style: TextStyle(color: Colors.white),
                                 ),
                                 Container(
                                   padding: const EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                                  decoration: const BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20)),
                                     color: Color.fromRGBO(93, 6, 138, 150),
                                   ),
-                                  child: Icon(
+                                  child: const Icon(
                                     Icons.arrow_forward_ios,
                                     color: Colors.white,
                                     size: 16,
