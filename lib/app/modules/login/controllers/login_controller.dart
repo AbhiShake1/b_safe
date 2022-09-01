7c514b26222c171106f33c2d0d88da607329ff01
abhi <abhi9848038283@gmail.com>
2022-09-01 11:57:14 +0545

    setup auto start code verification

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,6 +1,5 @@
 import 'package:b_safe/app/modules/login/repo/auth_repo.dart';
 import 'package:b_safe/app/modules/login/views/otp_view.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +7,8 @@ class LoginController extends GetxController {
   final _auth = Get.find<AuthRepo>();
 
   late final TextEditingController phoneNumberController, smsCodeController;
+
+  final _toStartVerification = false.obs;
 
   @override
   void onInit() {
@@ -28,14 +29,19 @@ class LoginController extends GetxController {
     super.onClose();
   }
 
+  void startCodeVerification() => _toStartVerification.value = true;
+
   Future<void> signInWithPhone() async {
     final result = await _auth.signInWithPhone(
       phoneNumberController.text,
       // ignore: avoid_redundant_argument_values
-      onCodeSent: (verificationId, __) => Get.snackbar(
-        'Code sent',
-        'OTP has been messaged to your number',
-      ),
+      onCodeSent: (verificationId, __) {
+        _toStartVerification.listen((start) {
+          if (start) {
+            _auth.verifyOtpAndSignIn(verificationId, smsCodeController.text);
+          }
+        });
+      },
     );
     result.fold(
       (l) => Get.to<dynamic>(const OtpView()),
